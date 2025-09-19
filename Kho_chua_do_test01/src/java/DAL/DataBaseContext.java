0f2892257de1720661efd15523c36c3ea9096998
Nguyễn Sơn Nam <Namhgjk123@gmail.com>
2025-09-19 21:57:29 +0700

    Thêm giao diện
    Tổng quan và Nhân viên

---
+++ Kho_chua_do_test01/src/java/DAL/DataBaseContext.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author TieuPham
+ */
+public class DataBaseContext {
+    protected Connection connection;
+    public DataBaseContext()
+    {
+        //@Students: You are allowed to edit user, pass, url variables to fit 
+        //your system configuration
+        //You can also add more methods for Database Interaction tasks. CRUD (Insert,Read,Updata,Delete)
+        //But we recommend you to do it in another class
+        // For example : StudentDBContext extends DBContext , 
+        //where StudentDBContext is located in dal package, 
+        try {
+            String user = "sa";
+            String pass = "123";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=MBP";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DataBaseContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
